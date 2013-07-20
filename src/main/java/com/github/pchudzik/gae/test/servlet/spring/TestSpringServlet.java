cf7c099db6d204ebc839ef2617641566e5b330f7
Paweł Chudzik <pawel.chudzik@gmail.com>
2013-07-20 10:34:33 +0200

    basic spring servlet

---
+++ src/main/java/com/github/pchudzik/gae/test/servlet/spring/TestSpringServlet.java
@@ -0,0 +1,25 @@
+package com.github.pchudzik.gae.test.servlet.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import static com.google.appengine.repackaged.com.google.common.base.Strings.nullToEmpty;
+
+/**
+ * User: pawel
+ * Date: 20.07.13
+ * Time: 10:20
+ */
+@Controller
+@RequestMapping("/spring")
+public class TestSpringServlet {
+
+	@RequestMapping(value = "/{hello}", method = RequestMethod.GET)
+	public String hello(@PathVariable String hello, ModelMap modelMap) {
+		modelMap.put("hello", nullToEmpty(hello));
+		return "helloSpring";
+	}
+}
---
+++ src/main/webapp/jsp/helloSpring.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: pawel
+  Date: 20.07.13
+  Time: 10:28
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<p>Hello from simple spring servlet</p>
+	<p>hello ${hello}</p>
+</body>
+</html>
